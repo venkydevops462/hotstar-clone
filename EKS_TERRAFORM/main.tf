801712f7084feebd91d2e54b19097fdf4ab1e38f
venkyvirat123 <venkataramanapodugu087@gmail.com>
2024-12-30 13:10:47 +0530

    files

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -51,7 +51,6 @@ resource "aws_eks_cluster" "example" {
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
   ]
@@ -102,7 +101,6 @@ resource "aws_eks_node_group" "example" {
   instance_types = ["t2.medium"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
