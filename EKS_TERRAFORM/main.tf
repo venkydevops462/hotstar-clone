0c9f61210272aa5c4b04c25b5b780b49b1a0ac15
venkyvirat123 <venkataramanapodugu087@gmail.com>
2024-12-30 12:34:19 +0530

    added files

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -49,7 +49,7 @@ resource "aws_eks_cluster" "example" {
 }
 
 resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
+  name = "eks-node-group-cloud1"
 
   assume_role_policy = jsonencode({
     Statement = [{
